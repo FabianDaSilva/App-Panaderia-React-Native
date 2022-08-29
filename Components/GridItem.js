dd1a7cada5e223e58a6bcf8609678abcf9cded0e
Fabian Da Silva <fabian.da.silva@hotmail.com>
2022-08-29 14:43:37 -0300

    100%

--- Components/GridItem.js
+++ Components/GridItem.js
@@ -1,44 +1,48 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react'
+import { 
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native'
 
-import React from 'react';
-
-const GridItem = ({item, onSelected}) => {
-	return (
-		<View style={styles.gridItem}>
-			<TouchableOpacity
-				onPress={() => onSelected(item)}
-				style={{...styles.container, backgroundColor: item.color}}
-			>
-				<View>
-					<Text style={styles.title}>{item.title}</Text>
-				</View>
-			</TouchableOpacity>
-		</View>
-	);
-};
+const GridItem = ({ item, onSelected}) => {
+    return (
+        <View style={styles.gridItem}>
+            <TouchableOpacity
+                onPress={() => onSelected(item)}
+                style={{...styles.container, backgroundColor: item.color}}
+            >
+                <View>
+                    <Text style={styles.title}>{item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-	gridItem: {
-		flex: 1,
-		borderRadius: 6,
-		margin: 15,
-		height: 150,
-	},
-	container: {
-		flex: 1,
-		borderRadius: 6,
-		shadowColor: 'Colors.neutralsOne',
-		shadowOffset: {width: 0, height: 2},
-		shadowOpacity: 0.8,
-		shadowRadius: 2,
-		elevation: 1,
-		padding: 10,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	title: {
-		fontFamily: 'IMedium',
-	},
-});
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
+        borderRadius: 6,
+    },
+    container: {
+        flex: 1,
+        borderRadius: 6,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        padding: 8,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        elevation: 3,
+    },
+    title: {
+        fontFamily: 'OpenSansBold',
+    }
+})
 
-export default GridItem;
+export default GridItem
--- Components/OrderItem.js
+++ Components/OrderItem.js
@@ -1,48 +1,48 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-import {COLORS} from '../constansts/Colors';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import React from 'react';
+import { COLORS } from '../constansts/Colors';
+import Ionicons from '@expo/vector-icons/Ionicons'
+import React from 'react'
 
 const formatDay = (time) => {
-	const date = new Date(time);
-	return date.toLocaleDateString();
-};
+    const date = new Date(time);
+    return date.toLocaleDateString();
+}
 
-const OrderItem = ({item, onDelete}) => {
-	return (
-		<View style={styles.order}>
-			<View>
-				<Text style={styles.date}>{formatDay(item.date)}</Text>
-				<Text style={styles.total}>${item.total}</Text>
-			</View>
-			<View style={styles.actions}>
-				<TouchableOpacity onPress={() => onDelete(item.id)}>
-					<Ionicons name="md-trash" color={COLORS.primary} size={22} />
-				</TouchableOpacity>
-			</View>
-		</View>
-	);
-};
+const OrderItem = ({ item, onDelete}) => {
+  return (
+    <View style={styles.order}>
+      <View>
+        <Text style={styles.date}>{formatDay(item.date)}</Text>
+        <Text style={styles.total}>${item.total}</Text>
+      </View>
+      <View style={styles.actions}>
+        <TouchableOpacity onPress={() => onDelete(item.id)}>
+            <Ionicons name='md-trash' color={COLORS.primary} size={22} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-     order: {
-         flexDirection: 'row',
-         justifyContent: 'space-between',
-         alignItems: 'center',
-         padding: 10,
-         margin: 10,
-         borderColor: '#ccc',
-         borderWidth: 1,
-         borderRadius: 6,
-     },
-     date: {
-         fontSize: 18
-     },
-     total: {
-         fontSize: 18,
-         fontFamily: 'IMedium'
-     }
- })
+    order: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        margin: 10,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 6,
+    },
+    date: {
+        fontSize: 18
+    },
+    total: {
+        fontSize: 18,
+        fontFamily: 'OpenSansBold'
+    }
+})
 
-export default OrderItem;
+export default OrderItem
